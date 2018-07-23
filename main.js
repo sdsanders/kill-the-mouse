3b0fec22388f17499bbc9aba75241cfdec34a5d1
Justin Schuldt <justinschuldt@gmail.com>
2018-07-22 20:04:50 -0500

    Add renderer.js, ipc, and react (#12)
    Fixes #2, #4, #5, #9

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const ioHook = require('iohook');
 const menubar = require('menubar');
 const moment = require('moment');
 const momentDurationFormatSetup = require('moment-duration-format');
+const url = require('url');
+const path = require('path');
+const { ipcMain } = require('electron');
 
 momentDurationFormatSetup(moment);
 
@@ -21,20 +24,29 @@ function handleKeyboardEvent(event) {
   }
 
   streak.keys++;
+  mb.window.webContents.send('current-streak', streak.keys);
 }
-
+let minStreak = 10;
 function handleMouseEvent(event) {
   if (!streak.active) { return };
 
   const currentTime = Date.now();
   const duration = moment.duration(currentTime - streak.startTime).format('h [hours], m [minutes], s [seconds]');
 
-  const notification = new Notification({
-    title: 'Streak broken!',
-    body: `You typed ${streak.keys} keys during that streak, lasting ${duration}`
-  });
+  if (streak.keys > minStreak) {
+    mb.window.webContents.send('streak-broken', {
+      keystrokes: streak.keys,
+      duration,
+      timestamp: Date.now()
+    });
 
-  notification.show();
+    const notification = new Notification({
+      title: 'Streak broken!',
+      body: `You typed ${streak.keys} keys during that streak, lasting ${duration}`
+    });
+
+    notification.show();
+  }
 
   streak.active = false;
   streak.keys = 0;
@@ -49,7 +61,19 @@ function registerShortcut() {
 }
 
 function ready () {
-  let index = 0;
+  mb.showWindow();
+}
+
+const windowRendered = () => {
+
+  mb.window.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // ✋ 🚫 🐛
+  // mb.window.openDevTools();
 
   ioHook.on('keyup', handleKeyboardEvent);
 
@@ -63,6 +87,22 @@ function ready () {
   globalShortcut.register('CommandOrControl+K', registerShortcut);
 }
 
+// startup health-check pings
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong');
+});
+
+ipcMain.on('min-streak', (event, arg) => {
+  console.log('new min-streak: ', arg);
+  minStreak = arg;
+});
+
+ipcMain.on('quit', (event, arg) => {
+  mb.app.quit();
+});
+
 mb.on('ready', ready);
 mb.on('after-show', () => { isMenuBarOpen = true; });
 mb.on('after-hide', () => { isMenuBarOpen = false; });
+mb.on('after-create-window', windowRendered);
---
+++ renderer.js
@@ -0,0 +1,93 @@
+const { ipcRenderer } = require('electron');
+
+// start up heath-check
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+ipcRenderer.send('asynchronous-message', 'ping');
+
+// front end
+const React = require('react');
+const ReactDOM = require('react-dom');
+const moment = require('moment');
+
+class MainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStreak: 0,
+      streaks: [],
+      minStreak: 10,
+    };
+
+    this.handleMinStreakChange = this.handleMinStreakChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.quit = this.quit.bind(this);
+    ipcRenderer.on('current-streak', this.currentStreak.bind(this));
+    ipcRenderer.on('streak-broken', this.streakBroken.bind(this));
+  }
+
+  handleMinStreakChange(event) {
+    this.setState({ minStreak: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    ipcRenderer.send('min-streak', this.state.minStreak);
+  }
+  currentStreak(event, arg) {
+    this.setState(() => ({ currentStreak: arg }))
+  }
+  
+  streakBroken(event, { keystrokes, duration, timestamp }) {
+    this.setState(() => ({
+      streaks: [
+        ...this.state.streaks,
+        {
+          keystrokes,
+          duration,
+          timestamp
+        }
+      ]
+    }));
+  }
+  quit() {
+    ipcRenderer.send('quit', true);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            minimum keystrokes for notification: 
+            <input type="number" value={this.state.minStreak} onChange={this.handleMinStreakChange} style={{width: 80}} />
+          </label>
+          <input type="submit" value="Set" />
+        </form>
+        <div>current streak: {this.state.currentStreak}</div>
+        <div>
+          <ul>
+            {this.state.streaks &&
+              this.state.streaks.length >= 1
+              ? (
+                <div>
+                  {this.state.streaks.sort((a, b) =>  b.keystrokes - a.keystrokes )
+                    .map((streak, index) => {
+                      return (
+                        <li key={index}>
+                          {streak.keystrokes} keys, {streak.duration}.  {moment(streak.timestamp).format("ddd, hA")}
+                        </li>
+                      )
+                    })}
+                </div>
+              ) : null}
+          </ul>
+        </div>
+        <button onClick={this.quit}>quit</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<MainComponent />, document.getElementById('container'));
